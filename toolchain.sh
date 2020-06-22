75f5241d51df4e1861ced0a64372366cc597e189
Wally4000 <catapultit@me.com>
2020-06-22 17:28:51 +1000

    Removed more redundant files.

--- toolchain.sh
+++ toolchain.sh
@@ -16,7 +16,7 @@ PSPSDK_URL="https://github.com/wally4000/pspsdk" #This is temporary until the SD
 NEWLIB_URL="https://github.com/NT-Bourgeois-Iridescence-Technologies/newlib"
 PSPLINK_URL="https://github.com/pspdev/psplinkusb"
 
-PREFIX="/usr/mipsel-sony-psp"
+PREFIX="$BASE_DIR/mipsel-sony-psp"
 
 
 function fetch_clang
@@ -34,10 +34,15 @@ function fetch_clang
 
 function prep_sources
 {
+    mkdir build; cd build
     git clone $RUST_URL
     git clone $PSPSDK_URL
     git clone $NEWLIB_URL
     git clone $PSPLINK_URL
+
+  #  mkdir -p "$PREFIX/psp/sdk/lib"
+    mkdir "$PREFIX/bin"
+    mkdir "$PREFIX/psp/share"
 }
 ## Configure Rust - This will fall into root
 function fetch_rust
@@ -46,52 +51,41 @@ function fetch_rust
 
     export PATH=$PATH:$HOME/.cargo/bin
     source $HOME/.cargo/env
-    rustup set profile complete
+    #rustup set profile complete
     rustup toolchain install nightly
-    rustup default nightly && rustup component add rust-src
+    rustup default nightly-2020-06-05 && rustup component add rust-src
     rustup update
-    cargo install cargo-psp
+    cargo install cargo-psp xargo
 }
 
 function compile_libpsp
 {
-    cargo install xargo
-
-    cat << EOF > rust-psp/psp/Xargo.toml
+    cat << EOF > $BUILD_DIR/rust-psp/psp/Xargo.toml
     [target.mipsel-sony-psp.dependencies.core]
     [target.mipsel-sony-psp.dependencies.alloc]
     [target.mipsel-sony-psp.dependencies.panic_unwind]
     stage = 1
 EOF
 
-    cd rust-psp/psp
+    cd $BUILD_DIR/rust-psp/psp
     xargo rustc --features stub-only --target mipsel-sony-psp -- -C opt-level=3 -C panic=abort
     cd $BUILD_DIR
 }
 
 function populateSDK
 {
-    #Setup Directories
-    mkdir -p "$PREFIX/psp/sdk/bin"
-    mkdir "$PREFIX/psp/bin" "$PREFIX/psp/sdk/include" "$PREFIX/psp/sdk/share" "$PREFIX/psp/sdk/lib"
-
-    cd pspsdk
+    cd $BUILD_DIR/pspsdk
     ./bootstrap
     mkdir build; cd build
     ../configure PSP_CC=clang PSP_CFLAGS="--config $PREFIX/psp/sdk/lib/clang.conf" PSP_CXX=clang++ PSP_AS=llvm-as PSP_LD=ld.lld PSP_AR=llvm-ar PSP_NM=llvm-nm PSP_RANLIB=llvm-ranlib --with-pspdev=$PREFIX --disable-sonystubs --disable-psp-graphics --disable-psp-libc
     make install-data
 
-    cp -r "$BASE_DIR/resources/cmake" "$PREFIX/psp/sdk/share"
-    cp "/root/.cargo/bin/pack-pbp" "$PREFIX/psp/sdk/bin"
-    cp "/root/.cargo/bin/mksfo" "$PREFIX/psp/sdk/bin"
-    cp "$BASE_DIR/build/rust-psp/target/mipsel-sony-psp/debug/libpsp.a" "$PREFIX/psp/sdk/lib"
-    
     cd $BUILD_DIR
 }
 
 function fetch_newlib
 {
-cd newlib
+cd $BUILD_DIR/newlib
 mkdir build && cd build
 CC=clang-$CLANG_VER ../configure AR_FOR_TARGET=llvm-ar-$CLANG_VER AS_FOR_TARGET=llvm-as-$CLANG_VER RANLIB_FOR_TARGET=llvm-ranlib-$CLANG_VER CC_FOR_TARGET=clang-$CLANG_VER CXX_FOR_TARGET=clang++-$CLANG_VER --target=psp --enable-newlib-iconv --enable-newlib-multithread --enable-newlib-mb --prefix=$PREFIX
 make -j6
@@ -104,7 +98,10 @@ function compileSDK
     cd $BUILD_DIR/pspsdk/build
     make && make install
     cd $BUILD_DIR
-    cp -r "$BASE_DIR/resources/lib" "$PREFIX/psp/sdk/"
+    cp -r "$BASE_DIR/resources/cmake" "$PREFIX/psp/share"
+    cp "$HOME/.cargo/bin/pack-pbp" "$PREFIX/bin"
+    cp "$HOME/.cargo/bin/mksfo" "$PREFIX/bin"
+    cp "$BUILD_DIR/rust-psp/target/mipsel-sony-psp/debug/libpsp.a" "$PREFIX/psp/sdk/lib"
 }
 
 function fetch_psplink
@@ -114,10 +111,8 @@ function fetch_psplink
     clang++-$CLANG_VER psplinkusb/pspsh/*.C -Ipsplinkusb/psplink -D_PCTERM -lreadline -lcurses -o $PREFIX/bin/pspsh
 }
 
-mkdir build && cd build
-
 prep_sources
-fetch_clang
+#fetch_clang
 fetch_rust
 compile_libpsp
 populateSDK
