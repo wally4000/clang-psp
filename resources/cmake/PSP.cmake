7a76ceb1ecd45857bc3b421690ee1e0a057a8d30
Wally4000 <catapultit@me.com>
2020-08-03 09:51:40 +1000

    Switch to $PSPDEV / $PSPSDK variables for paths instead of hardcoded paths as SDK will be independant of location

--- resources/cmake/PSP.cmake
+++ resources/cmake/PSP.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR mips)
 
 set(triple mips-unknown-linux)
-set (CMAKE_SYSROOT /usr/mipsel-sony-psp/psp)
+set (CMAKE_SYSROOT $ENV{PSPDEV}/psp)
 
 
 set(CMAKE_C_COMPILER clang)
@@ -10,6 +10,6 @@ set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
-set(CMAKE_C_FLAGS "--config /usr/mipsel-sony-psp/psp/sdk/lib/clang.conf -fgnuc-version=0")
-set(CMAKE_CXX_FLAGS "--config /usr/mipsel-sony-psp/psp/sdk/lib/clang.conf -fgnuc-version=0" )
+set(CMAKE_C_FLAGS "--config $ENV{PSPSDK}/lib/clang.conf -fgnuc-version=0")
+set(CMAKE_CXX_FLAGS "--config $ENV{PSPSDK}/lib/clang.conf -fgnuc-version=0" )
 
